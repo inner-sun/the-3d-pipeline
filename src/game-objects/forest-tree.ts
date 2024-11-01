19c5bd11b879d5d78f3bb58d28e78ad7646dd892
InnerSun <167511709+inner-sun@users.noreply.github.com>
2024-11-01 02:36:22 +0100

    Added Tree asset

---
+++ src/game-objects/forest-tree.ts
@@ -0,0 +1,20 @@
+import GameEngine from '~/game-engine'
+import GameObject from '~/game-objects/game-object'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import model from '~/assets/tree.glb?url'
+
+export default class ForestTree extends GameObject{
+  constructor(){
+    super()
+
+    const loader = new GLTFLoader()
+    loader.load(model, (mesh) => {
+      console.log(mesh)
+      this.meshGroup.add(mesh.scene)
+    })
+  }
+
+  tick(engine: GameEngine){
+
+  }
+}
